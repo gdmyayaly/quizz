b52ccbca665a6b381dc48ed656c4b6faddb0d884
mbacke <elhadjimbackembaye@gmail.com>
2020-01-27 09:54:46 +0000

    finalisation dashboard55555555555555

--- page/con.php
+++ page/con.php
@@ -1,6 +1,6 @@
 <?php
     $server="localhost";
     $login="root";
-    $pass="welcome";
+    $pass="1lovem@ty";
     $connexion = new PDO("mysql:host=$server;dbname=grow",$login,$pass);
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
